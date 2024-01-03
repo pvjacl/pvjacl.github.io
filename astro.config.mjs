e29842a0579b407ead0fadb38d6fa0926b0afb77
Brian Yamasaki <brian.yamasaki@gmail.com>
2024-01-02 16:45:22 -0800

    Remove Tailwinds fix styling for front page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), tailwind()]
+  integrations: [mdx(), sitemap(), react()]
 });
