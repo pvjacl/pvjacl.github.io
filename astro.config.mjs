c6019fbede95144518521a727c7588385bbcc9df
Brian Yamasaki <brian.yamasaki@gmail.com>
2024-01-04 22:52:57 -0800

    build in /docs directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pvjacl.github.io',
-  integrations: [mdx(), sitemap(), react()]
+  integrations: [mdx(), sitemap(), react()],
+  outDir: 'docs'
 });
