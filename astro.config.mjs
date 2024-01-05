a291f1133ec986661639fe349babbe34702d4af8
Brian Yamasaki <brian.yamasaki@gmail.com>
2024-01-04 23:03:06 -0800

    add base to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pvjacl.github.io',
+  base: '/pvjacl',
   integrations: [mdx(), sitemap(), react()],
   outDir: 'docs'
 });
