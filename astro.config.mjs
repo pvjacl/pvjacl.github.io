dd72df689acdedd99716a66300e466bd30f59be6
Brian Yamasaki <brian.yamasaki@gmail.com>
2024-06-01 11:30:58 -0700

    change to puyallupvalleyjacl.org in astro.conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://m.puyallupvalleyjacl.org',
+  site: 'https://puyallupvalleyjacl.org',
   integrations: [
     mdx(), 
     sitemap({
