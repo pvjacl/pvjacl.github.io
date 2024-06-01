1827b5513c8cbdc064772602c86dd728e344b16b
Brian Yamasaki <brian.yamasaki@gmail.com>
2024-06-01 11:08:12 -0700

    test to see if changing canonical endpoint will fix redirect issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pvjacl.github.io',
+  site: 'https://m.puyallupvalleyjacl.org',
   integrations: [
     mdx(), 
     sitemap({
