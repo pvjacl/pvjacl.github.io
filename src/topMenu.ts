7a898910be62549b2df0194aecd00f0b9aa6b59b
brianyamasaki <brian.yamasaki@gmail.com>
2025-02-13 19:22:09 -0800

    styling changes for 2025 Redesign Hamburger menu support for narrow screens separated menu structures Tabs to go between old and new parts of web page

--- src/topMenu.ts
+++ src/topMenu.ts
@@ -144,3 +144,157 @@ export const menubar: MenuItemData[] = [
   },
 ];
 
+export const galleryMenubar: MenuItemData[] = [
+  {
+    id: 'home',
+    isDropdown: false,
+    src: '/gallery',
+    label: 'Home',
+    children: []
+  },
+  {
+    id: 'remGalleryAboutMenu',
+    isDropdown: true,
+    src: '',
+    label: 'About',
+    children: [
+      {
+        id: 'remGalleryAboutGallery',
+        isDropdown: false,
+        src: '/gallery/about',
+        label: 'About the Gallery',
+        children: []
+      },
+      {
+        id: 'remGalleryHistory',
+        isDropdown: false,
+        src: '/history/assembly-history',
+        label: 'History',
+        children: []
+      },
+      {
+        id: 'remGalleryTeam',
+        isDropdown: false,
+        src: '/gallery/gallery-team',
+        label: 'Our Team',
+        children: []
+      },
+      {
+        id: 'remGalleryBrochure',
+        isDropdown: false,
+        src: '/galleryAssets/RG-Brochure_FINAL.pdf',
+        label: 'Brochure (PDF)',
+        children: []
+      },
+    ]
+  },
+  {
+    id: 'remIncarcerees',
+    isDropdown: true,
+    src: '',
+    label: 'Incarcerees',
+    children: [
+      {
+        id: 'remNames',
+        isDropdown: false,
+        src: '/gallery/gallery-names',
+        label: 'Name Search',
+        children: []
+      }
+    ]
+  },
+  {
+    id: 'remLearn',
+    isDropdown: true,
+    src: '',
+    label: 'Learn More',
+    children: [
+      {
+        id: 'remSilentFair',
+        isDropdown: false,
+        src: '/history/the-silent-fair',
+        label: 'The Silent Fair Documentary',
+        children: []
+      },
+      {
+        id: 'remKindness',
+        isDropdown: false,
+        src: '/history/gallery-acts-of-kindness',
+        label: 'Acts of Kindness',
+        children: []
+      },
+      {
+        id: 'remHarmonySculpture',
+        isDropdown: false,
+        src: '/history/harmony-sculpture',
+        label: 'Harmony (sculpture)',
+        children: []
+      },
+      {
+        id: 'remResources',
+        isDropdown: false,
+        src: '/history/additional-resources',
+        label: 'Additional Resources',
+        children: []
+      },
+    ]
+  },
+  {
+    id: 'remSupport',
+    isDropdown: true,
+    src: '',
+    label: 'Support Us',
+    children: [
+      {
+        id: 'remGalleryDonate',
+        isDropdown: false,
+        src: '/gallery/gallery-donate',
+        label: 'Donate',
+        children: []
+      },
+      {
+        id: 'remVolunteer',
+        isDropdown: false,
+        src: '/gallery/gallery-volunteer',
+        label: 'Volunteer',
+        children: []
+      },
+      {
+        id: 'remGalleryDonors',
+        isDropdown: false,
+        src: '/gallery/donors',
+        label: 'Donors & Endorsements',
+        children: []
+      },
+    ]
+  },
+  {
+    id: 'remVisit',
+    isDropdown: true,
+    src: '',
+    label: 'Visit Us',
+    children: [
+      {
+        id: 'remTours',
+        isDropdown: false,
+        src: '/gallery/gallery-tours',
+        label: 'Tours & Presenations',
+        children: []
+      },
+      {
+        id: 'remEvents',
+        isDropdown: false,
+        src: '/gallery/gallery-events',
+        label: 'Events',
+        children: []
+      },
+      {
+        id: 'remLocation',
+        isDropdown: false,
+        src: '/gallery/gallery-location',
+        label: 'Location & Hours',
+        children: []
+      },
+    ]
+  },
+]
